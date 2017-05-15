98a3f114d5a527eeee0bc5232c1b4c6a03eb9ebe
Joseph Bluck <bluckjoseph@gmail.com>
2017-05-15 19:34:23 +0100

    Sprite Class Implemented

---
+++ SDL Template/include/Sprite.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "SDLInit.h"
+
+class Sprite
+{
+public:
+	Sprite(SDL_Renderer* _renderer, char* _fileName, int _x, int _y, int _w, int _h);
+	~Sprite();
+
+	virtual void Draw(); //Draw sprite with previous parameters
+	virtual void Draw(int _x, int _y); //Draw sprite at set position
+	virtual void Draw(int _x, int _y, SDL_RendererFlip _flip); //Draw sprite at set position with a flip
+	virtual void Draw(int _x, int _y, int _angle, SDL_RendererFlip _flip); //Draw with position, flip and rotation
+
+	//Data Retrieval Commands
+	SDL_Rect& GetRect() { return posRect; } //Returns positional rectangle of the sprite
+	
+	//Position and size values
+	int GetX() { return posRect.x; }
+	int GetY() { return posRect.y; }
+	int GetW() { return posRect.w; }
+	int GetH() { return posRect.h; }
+
+protected:
+	SDL_Renderer* renderer;
+	SDL_Texture* tex;
+	SDL_Rect posRect;
+
+	SDL_RendererFlip flip; //flip variable
+
+	int angle; //Angle of rotation
+
+	bool isValid; //Control variable that prevents memory access violations
+};
---
+++ SDL Template/src/Sprite.cpp
@@ -0,0 +1,74 @@
+#include "Sprite.h"
+
+Sprite::Sprite(SDL_Renderer* _renderer, char* _fileName, int _x, int _y, int _w, int _h)
+{
+	//Initialise all pointers as NULL to prevent errors
+	renderer = NULL;
+	tex = NULL;
+
+	//Setup the SDL Rect
+	posRect.x = _x;
+	posRect.y = _y;
+	posRect.h = _h;
+	posRect.w = _w;
+
+	flip = SDL_FLIP_NONE; //Set flipping to none by default
+	angle = 0; //Set rotation to none by default
+
+	renderer = _renderer; //Assign parameters into values
+	tex = IMG_LoadTexture(renderer, _fileName);
+
+	if (renderer == NULL || tex == NULL) {
+		std::cout << "Sprite failed to load: " << IMG_GetError() << "\n";
+		isValid = false; //Renderer or filepath was invalid, sprite is now invalidated
+		return;
+	}
+
+	isValid = true; //All checks came back fine, sprite is now safe to use
+}
+
+Sprite::~Sprite()
+{
+	if (tex != NULL) { SDL_DestroyTexture(tex); }
+}
+
+//Draw Functions
+void Sprite::Draw()
+{
+	if (isValid) { //Check if sprite is valid before drawing
+		SDL_RenderCopyEx(renderer, tex, NULL, &posRect, angle, NULL, flip);
+	}
+}
+
+void Sprite::Draw(int _x, int _y)
+{
+	if (isValid) { //Check if sprite is valid before drawing
+		posRect.x = _x; //Update Position
+		posRect.y = _y;
+
+		SDL_RenderCopyEx(renderer, tex, NULL, &posRect, angle, NULL, flip);
+	}
+}
+
+void Sprite::Draw(int _x, int _y, SDL_RendererFlip _flip)
+{
+	if (isValid) { //Check if sprite is valid before drawing
+		posRect.x = _x; //Update Position and flip parameter
+		posRect.y = _y;
+		flip = _flip;
+
+		SDL_RenderCopyEx(renderer, tex, NULL, &posRect, angle, NULL, flip);
+	}
+}
+
+void Sprite::Draw(int _x, int _y, int _angle, SDL_RendererFlip _flip)
+{
+	if (isValid) { //Check if sprite is valid before drawing
+		posRect.x = _x; //Update Position and flip parameter
+		posRect.y = _y;
+		flip = _flip;
+		angle = _angle;
+
+		SDL_RenderCopyEx(renderer, tex, NULL, &posRect, angle, NULL, flip);
+	}
+}
